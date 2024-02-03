d109f7bcbeb29abc678ffe8209fc4dc1959616fb
Abdul Qader <gm.abdul.qader@gmail.com>
2024-02-04 01:15:25 +0600

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,80 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import phpLogo from "./tech/php-Logo.svg";
+import mysqlLogo from "./tech/mysqlLogo.png";
+import pythonLogo from "./tech/pythonLogo.png";
+import allureLogo from "./tech/allureLogo.png";
+import javaLogo from "./tech/javaLogo.png";
+import jenkinsLogo from "./tech/jenkinsLogo.svg";
+import djangoLogo from "./tech/djangoLogo.svg";
+import flutterLogo from "./tech/icon_flutter.png";
+
+import gigalinkLogo from "./company/gigalink.png";
+import ipinfraLogo from "./company/ipinfraLogo.jpg";
+import a1qaLogo from "./company/a1qaLogo.svg";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import restaurantWebsite from "./restaurantWebsite.png";
+import gptWebsite from "./gptWebsite.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  ipinfraLogo,
+  gigalinkLogo,
+  a1qaLogo,
+  carrent,
+  jobit,
+  tripguide,
+  phpLogo,
+  mysqlLogo,
+  allureLogo,
+  jenkinsLogo,
+  pythonLogo,
+  djangoLogo,
+  javaLogo,
+  flutterLogo,
+  restaurantWebsite,
+  gptWebsite,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,285 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  gigalinkLogo,
+  ipinfraLogo,
+  a1qaLogo,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  phpLogo,
+  mysqlLogo,
+  allureLogo,
+  jenkinsLogo,
+  pythonLogo,
+  djangoLogo,
+  javaLogo,
+  flutterLogo,
+  gptWebsite,
+  restaurantWebsite,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Js Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "QA Software Tester",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "Java",
+    icon: javaLogo,
+  },
+  {
+    name: "Python",
+    icon: pythonLogo,
+  },
+  {
+    name: "Django",
+    icon: djangoLogo,
+  },
+  {
+    name: "Flutter",
+    icon: flutterLogo,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "php",
+    icon: phpLogo,
+  },
+  {
+    name: "MySQL",
+    icon: mysqlLogo,
+  },
+  {
+    name: "Allure",
+    icon: allureLogo,
+  },
+  {
+    name: "Jenkins",
+    icon: jenkinsLogo,
+  },
+];
+
+const experiences = [
+  {
+    title: "Software Developer Intern",
+    company_name: "Gigalink Solutions Sdn Bhd",
+    icon: gigalinkLogo,
+    iconBg: "#E6DEDD",
+    date: "Mar 2022 - Aug 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Software Developer",
+    company_name: "Ipinfra Networks Sdn Bhd",
+    icon: ipinfraLogo,
+    iconBg: "#E6DEDD",
+    date: "Sep 2021 - Aug 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "QA Test Automatin Engineer",
+    company_name: "A1QA",
+    icon: a1qaLogo,
+    iconBg: "#E6DEDD",
+    date: "Nov 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/AbdulQader496/car_rent_website",
+  },
+  {
+    name: "Web GPT",
+    description:
+      "Web application that enables GPT to promote new AI which acts as a patform for new text based AI. It shows the use for different view to make a website with react.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: gptWebsite,
+    source_code_link: "https://github.com/AbdulQader496/react-web-gpt",
+  },
+  {
+    name: "Restaurant wensite",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: restaurantWebsite,
+    source_code_link: "https://github.com/AbdulQader496/restaurant_web_react",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
